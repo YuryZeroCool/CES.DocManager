0b3c3144147ffc375ccbf41c73e5ae0dc9706b95
Tatsiana Bivoina <tanya.urainbow@mail.ru>
2022-10-14 20:22:37 +0300

    fix: override mui components styles on fuel report page

--- CES.DocManager.WebApi/ClientApp/src/routes/fuelReport/FuelReportPage.component.tsx
+++ CES.DocManager.WebApi/ClientApp/src/routes/fuelReport/FuelReportPage.component.tsx
@@ -10,7 +10,7 @@ import './FuelReportPage.style.scss';
 import { Division, IAllDivisionWorkSchedulesResponse, ICreateDivisionWorkScheduleResponse } from '../../types/ReportTypes';
 
 const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-const weekDays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+const weekDays = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
 
 interface Props {
   dates: DateObject[];
@@ -45,61 +45,69 @@ export default function FuelReportPageComponent(props: Props) {
 
   return (
     <section className="report-page-section">
-      <h2>Топливный отчет</h2>
-      <h4 className="calendar-form-title">Выберите график работы смен</h4>
-      <div className="error-message-container">
-        {workScheduleError ? <p className="error-message">{workScheduleError}</p> : <p> </p>}
-      </div>
-      <div className="calendar-form">
-        <Calendar
-          className="calendar"
-          value={dates}
-          onChange={onChange}
-          format="YYYY-MM-DD"
-          months={months}
-          weekDays={weekDays}
-          multiple
-          plugins={[
-            <DatePanel header="Даты" />,
-          ]}
-        />
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Смена</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={currentDivision}
-            label="Смена"
-            onChange={handleChange}
-          >
-            {divisions.map((el: Division) => (
-              <MenuItem key={el.id} value={el.division}>{el.division}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <div className="saved-data">
-          {allDivisionWorkScheduleState
-          && allDivisionWorkScheduleState?.length !== 0 && (
-            allDivisionWorkScheduleState.map((el: ICreateDivisionWorkScheduleResponse) => (
-              <div key={el.id} className="saved-data-container">
-                {el.division && el.dates && (
-                  <p>{`${el.division}: ${createDatesStr(el.dates)}`}</p>
-                )}
-                { /* eslint-disable-next-line @typescript-eslint/no-misused-promises */ }
-                <DeleteIcon className="icon-delete" onClick={() => handeDeleteElement(el.id)} />
-              </div>
-            ))
-          )}
+      <div className="wrapper">
+        <h2>Топливный отчет</h2>
+        <h4 className="calendar-form-title">Выберите график работы смен</h4>
+        <div className="error-message-container">
+          {workScheduleError ? <p className="error-message">{workScheduleError}</p> : <p> </p>}
         </div>
-      </div>
-      <div className="calendar-form-button-block">
-        <Button
-          variant="outlined"
-          disabled={!(isDivisionSelected && isDatesSelected)}
-          onClick={handleClick}
-        >
-          Сохранить
-        </Button>
+        <div className="division-work-schedule-block">
+          <div className="calendar-form">
+            <div className="calendar-block">
+              <Calendar
+                className="calendar"
+                value={dates}
+                onChange={onChange}
+                format="YYYY-MM-DD"
+                months={months}
+                weekDays={weekDays}
+                multiple
+                plugins={[
+                  <DatePanel header="Даты" />,
+                ]}
+              />
+              <FormControl className="divisions-select-control">
+                <InputLabel id="demo-simple-select-label">Смена</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={currentDivision}
+                  label="Смена"
+                  className="divisions-select"
+                  onChange={handleChange}
+                >
+                  {divisions.map((el: Division) => (
+                    <MenuItem key={el.id} value={el.division}>{el.division}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+            <div className="calendar-form-button-block">
+              <Button
+                className="calendar-form-button"
+                variant="outlined"
+                disabled={!(isDivisionSelected && isDatesSelected)}
+                onClick={handleClick}
+              >
+                Сохранить
+              </Button>
+            </div>
+          </div>
+          <div className="saved-data">
+            {allDivisionWorkScheduleState
+            && allDivisionWorkScheduleState?.length !== 0 && (
+              allDivisionWorkScheduleState.map((el: ICreateDivisionWorkScheduleResponse) => (
+                <div key={el.id} className="saved-data-container">
+                  {el.division && el.dates && (
+                    <p>{`${el.division}: ${createDatesStr(el.dates)}`}</p>
+                  )}
+                  { /* eslint-disable-next-line @typescript-eslint/no-misused-promises */ }
+                  <DeleteIcon className="icon-delete" onClick={() => handeDeleteElement(el.id)} />
+                </div>
+              ))
+            )}
+          </div>
+        </div>
       </div>
     </section>
   );
