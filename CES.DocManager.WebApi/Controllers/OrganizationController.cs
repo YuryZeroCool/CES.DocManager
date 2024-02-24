6c11b5efc7c0657995224091666cea673834bbc2
Yury Kishkurna <Kishkurno.yury@yandex.by>
2024-02-24 21:27:51 +0300

    add OrganizationController

---
+++ CES.DocManager.WebApi/Controllers/OrganizationController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CES.DocManager.WebApi.Controllers
+{
+    [EnableCors("MyPolicy")]
+    [Route("mes/organization")]
+    [ApiController]
+    public class OrganizationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        private readonly IMapper _mapper;
+
+        public OrganizationController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+    }
+}
