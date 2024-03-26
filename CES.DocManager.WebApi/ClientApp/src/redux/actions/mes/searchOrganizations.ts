0667a8dfeefb79f3e3f79337d5ff53276e571926
Yury Kishkurna <Kishkurno.yury@yandex.by>
2024-03-26 21:27:22 +0300

    rename env variable

--- CES.DocManager.WebApi/ClientApp/src/redux/actions/mes/searchOrganizations.ts
+++ CES.DocManager.WebApi/ClientApp/src/redux/actions/mes/searchOrganizations.ts
@@ -8,11 +8,11 @@ SearchOrganization, { rejectValue: FetchTodosError }>(
   'searchOrganizations',
   async (params, { rejectWithValue }) => {
     try {
-      if (process.env.REACT_APP_SEARCH_ORGANIZATIONS === undefined) {
+      if (process.env.REACT_APP_GET_ORGANIZATIONS === undefined) {
         throw Error('Упс, что-то пошло не так...');
       }
       const response = await $api.get<ISearchOrganization>(
-        `${process.env.REACT_APP_SEARCH_ORGANIZATIONS}?title=${params.title}&limit=${params.limit}&page=${params.page}`,
+        `${process.env.REACT_APP_GET_ORGANIZATIONS}?title=${params.title}&limit=${params.limit}&page=${params.page}`,
       );
       return response.data;
     } catch (err) {
