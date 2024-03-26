00b36a4bafdbee88774f3523022454b936f92c60
Yury Kishkurna <Kishkurno.yury@yandex.by>
2024-03-26 21:19:35 +0300

    fix house number

--- CES.Domain/Models/Response/Mes/HouseNumbers/CreateHouseNumberResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CES.Domain.Models.Response.Mes.HouseNumbers
-{
-    public class CreateHouseNumberResponse
-    {
-        public int Id { get; set; }
-
-        public string? HouseNumber { get; set; }
-    }
-}
