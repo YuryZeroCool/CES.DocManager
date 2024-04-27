d0afd3f034d81f277612852e32fda11b292a1837
Yury Kishkurna <Kishkurno.yury@yandex.by>
2024-04-27 15:00:58 +0300

    fix: move models to mes folder

--- CES.DocManager.WebApi/Models/Mes/EditExistedNoteViewModel.cs
+++ CES.DocManager.WebApi/Models/Mes/EditExistedNoteViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace CES.DocManager.WebApi.Models
+﻿using CES.DocManager.WebApi.Models.Mes;
+
+namespace CES.DocManager.WebApi.Models
 {
     public class EditExistedNoteViewModel : NoteViewModel
     {
+        public int Id { get; set; }
+
        public ContactInfoViewModel[]? NoteContactsInfo { get; set; }
     }
 }
