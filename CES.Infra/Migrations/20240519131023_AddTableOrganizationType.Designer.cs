963bda9bde529d8ce37bab6413a62a58334a6078
Yury Kishkurna <Kishkurno.yury@yandex.by>
2024-05-19 20:23:50 +0300

    feat: add table organization type. model

--- CES.Infra/Migrations/20240519131023_AddTableOrganizationType.Designer.cs
+++ CES.Infra/Migrations/20240519131023_AddTableOrganizationType.Designer.cs
@@ -4,6 +4,7 @@ using CES.Infra;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CES.Infra.Migrations
 {
     [DbContext(typeof(DocMangerContext))]
-    partial class DocMangerContextModelSnapshot : ModelSnapshot
+    [Migration("20240519131023_AddTableOrganizationType")]
+    partial class AddTableOrganizationType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -602,9 +604,6 @@ namespace CES.Infra.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(450)");
 
-                    b.Property<int?>("OrganizationTypeId")
-                        .HasColumnType("int");
-
                     b.Property<string>("PayerAccountNumber")
                         .HasColumnType("nvarchar(max)");
 
@@ -615,8 +614,6 @@ namespace CES.Infra.Migrations
 
                     b.HasIndex("Name")
                         .IsUnique();
-
-                    b.HasIndex("OrganizationTypeId");
 
                     b.ToTable("OrganizationEntities");
                 });
@@ -934,15 +931,6 @@ namespace CES.Infra.Migrations
                     b.Navigation("Street");
                 });
 
-            modelBuilder.Entity("CES.Infra.Models.Mes.OrganizationEntity", b =>
-                {
-                    b.HasOne("CES.Infra.Models.Mes.OrganizationTypeEntity", "OrganizationType")
-                        .WithMany("Organizations")
-                        .HasForeignKey("OrganizationTypeId");
-
-                    b.Navigation("OrganizationType");
-                });
-
             modelBuilder.Entity("CES.Infra.Models.NumberPlateOfCarEntity", b =>
                 {
                     b.HasOne("CES.Infra.Models.VehicleModelEntity", "VehicleModel")
@@ -1020,11 +1008,6 @@ namespace CES.Infra.Migrations
             modelBuilder.Entity("CES.Infra.Models.Mes.ActEntity", b =>
                 {
                     b.Navigation("Notes");
-                });
-
-            modelBuilder.Entity("CES.Infra.Models.Mes.OrganizationTypeEntity", b =>
-                {
-                    b.Navigation("Organizations");
                 });
 
             modelBuilder.Entity("CES.Infra.Models.NumberPlateOfCarEntity", b =>
---
+++ CES.Infra/Migrations/20240519132811_addOne-To-ManyOrganizationTypeLink.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CES.Infra.Migrations
+{
+    public partial class addOneToManyOrganizationTypeLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrganizationTypeId",
+                table: "OrganizationEntities",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrganizationEntities_OrganizationTypeId",
+                table: "OrganizationEntities",
+                column: "OrganizationTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrganizationEntities_OrganizationTypes_OrganizationTypeId",
+                table: "OrganizationEntities",
+                column: "OrganizationTypeId",
+                principalTable: "OrganizationTypes",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrganizationEntities_OrganizationTypes_OrganizationTypeId",
+                table: "OrganizationEntities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrganizationEntities_OrganizationTypeId",
+                table: "OrganizationEntities");
+
+            migrationBuilder.DropColumn(
+                name: "OrganizationTypeId",
+                table: "OrganizationEntities");
+        }
+    }
+}
