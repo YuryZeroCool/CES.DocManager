be331bf7d5d55e28366677266f19a714053a1123
Yury Kishkurna <Kishkurno.yury@yandex.by>
2024-08-24 13:49:51 +0300

    feat: add unit models

---
+++ CES.Domain/Models/Request/MaterialReport/GetUnitsRequest.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace CES.Domain.Models.Request.MaterialReport
+{
+    public class GetUnitsRequest : IRequest<List<CES.Domain.Models.Unit>>
+    {}
+}
---
+++ CES.Domain/Models/Unit.cs
@@ -0,0 +1,9 @@
+﻿namespace CES.Domain.Models
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
