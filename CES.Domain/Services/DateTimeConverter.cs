26aeb15937cb6fac9cfb09e0602b006cebe049d3
Yury Kishkurna <Kishkurno.yury@yandex.by>
2024-08-25 15:54:31 +0300

    fix: date time

---
+++ CES.Domain/Services/DateTimeConverter.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace CES.Domain.Services
+{
+    public class DateTimeConverter
+    {
+        public static DateTime ConvertToDateTime(string inputDate, string targetFormat)
+        {
+            string[] formats = {
+            "yyyy.MM.dd HH:mm:ss",
+            "dd.MM.yyyy HH:mm:ss",
+            "MM/dd/yyyy HH:mm:ss",
+            "dd/MM/yyyy HH:mm:ss",
+            "yyyy-MM-ddTHH:mm:ss",
+            "yyyy-MM-dd HH:mm:ss",
+            "dd-MM-yyyy HH:mm:ss",
+            "MM-dd-yyyy HH:mm:ss",
+            "dd.MM.yyyy",
+            "yyyy.MM.dd",
+            "MM/dd/yyyy",
+            "dd/MM/yyyy",
+            "yyyy-MM-dd",
+            "dd-MM-yyyy",
+            "MM-dd-yyyy"
+        };
+
+            DateTime parsedDate;
+
+            // Попытка распарсить дату с учетом форматов
+            bool success = DateTime.TryParseExact(inputDate, formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate);
+
+            if (success)
+            {
+                // Попытка вернуть дату, отформатированную в targetFormat, но в виде DateTime
+                string formattedDate = parsedDate.ToString(targetFormat);
+                DateTime resultDate;
+                if (DateTime.TryParseExact(formattedDate, targetFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out resultDate))
+                {
+                    return resultDate;
+                }
+                else
+                {
+                    throw new FormatException("Failed to format the date to the target format.");
+                }
+            }
+            else
+            {
+                throw new FormatException("The provided date string does not match any of the expected formats.");
+            }
+        }
+    }
+}
